481eaac632a367ae4625058d262af820fbd98502
Deniz Sokullu <deniz.sokullu@modolabs.com>
2022-02-09 10:57:38 -0500

    add login script

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+async function run () {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto('https://www.dominion.games');
+
+    await page.waitForSelector(".login-form", { timeout: 5000 });
+
+    await page.waitForTimeout(2000);
+
+    await page.type('#username-input', 'dominionstatswatcher');
+    await page.type(`input[type='password']`, ''); // get password from ENV
+    await page.click(`input[value='Login']`);
+
+    await page.waitForTimeout(2000);
+
+    await page.screenshot({path: 'login.png'});
+
+    browser.close();
+}
+
+run();
